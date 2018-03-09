77a9d6964e8cc0b85e067e30b9540db20f29e97b
Francisco Ilagan <Kiko@DN51r859.SUNet>
2018-03-09 13:54:44 -0800

    removed debugging print statment

--- vision/model/training.py
+++ vision/model/training.py
@@ -54,7 +54,6 @@ def train_sess(sess, model_spec, num_steps, writer, params):
         if i % params.save_summary_steps == 0:
             # Perform a mini-batch update
             x_hat, global_step_val = sess.run([compress, global_step])
-            print(x_hat.shape)
             _, _, residual = sess.run([rec_train_op, rec_loss, rec], feed_dict={x_hat_feed:x_hat})
             print("got rec output")
             _, _, com_output = sess.run([com_train_op, com_loss, com], feed_dict={rec_output:residual})
